7a5f64cf5df476da7a78ac570df7579c9b8f6d49
MaratVa <ItMaratkabest@yandex.ru>
2024-08-23 19:58:09 +0300

    L-1-1 create smth

---
+++ src/main/kotlin/lesson_1/lesson_1_1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val sequenceNumber = 75
+    val thankYou = "Спасибо за быструю доставку"
+
+    println(sequenceNumber)
+    println(thankYou)
+}
